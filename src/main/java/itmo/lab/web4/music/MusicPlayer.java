32eb74418d766bddc671fc320e79ff72ddf242d1
cyb <109616806+DopleZZ@users.noreply.github.com>
2025-04-17 13:15:07 +0300

    music

--- src/main/java/itmo/lab/web4/music/MusicPlayer.java
+++ src/main/java/itmo/lab/web4/music/MusicPlayer.java
@@ -1,7 +1,9 @@
+package itmo.lab.web4.music;
+
+import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.File;
 
 public class MusicPlayer {
     public static void main(String[] args) {
@@ -10,15 +12,12 @@ public class MusicPlayer {
             return;
         }
 
-        String audioFilePath = args[0];
         try {
-            File audioFile = new File(audioFilePath);
+            File audioFile = new File(args[0]);
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start();
-
-            System.out.println("Playing: " + audioFilePath);
             Thread.sleep(clip.getMicrosecondLength() / 1000);
         } catch (Exception e) {
             e.printStackTrace();
